d1c670b24c1590f493aa3131afefc3492ce00025
Yu-Konishi <yu.konishi@rakus.co.jp>
2020-07-09 15:02:13 +0900

    コメントのフォームに入力チェックを追加

--- src/main/java/com/example/form/CommentForm.java
+++ src/main/java/com/example/form/CommentForm.java
@@ -1,5 +1,8 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * コメントのフォーム.
  * 
@@ -10,22 +13,25 @@ public class CommentForm {
 	/**
 	 * ID
 	 */
-	private String id;
+	private String articleId;
 	/**
 	 * 名前
 	 */
+	@NotBlank(message="名前を入力してください")
+	@Size(min=1, max=50, message="名前は50文字以下で入力してください")
 	private String name;
 	/**
 	 * 内容
 	 */
+	@NotBlank(message="内容を入力してください")
 	private String content;
 
-	public String getId() {
-		return id;
+	public String getArticleId() {
+		return articleId;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
 	}
 
 	public String getName() {
