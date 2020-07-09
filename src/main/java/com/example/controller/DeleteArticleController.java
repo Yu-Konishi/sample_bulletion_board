b670b421159526d0f037005e319d3580227451bc
Yu-Konishi <yu.konishi@rakus.co.jp>
2020-07-09 14:32:51 +0900

    記事とそれに関連するコメントを削除するコントローラを作成

---
+++ src/main/java/com/example/controller/DeleteArticleController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.ArticleRepository;
+import com.example.repository.CommentRepository;
+
+@Controller
+@RequestMapping("/delete")
+public class DeleteArticleController {
+
+	/**
+	 * 記事のリポジトリ
+	 */
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	/**
+	 * コメントのリポジトリ
+	 */
+	@Autowired
+	private CommentRepository commentRepository;
+
+	/**
+	 * 記事とそれに関連するコメントを削除.
+	 * 
+	 * @param articleId ID
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("")
+	public String delete(Integer articleId) {
+		commentRepository.deleteByArticleId(articleId);
+		articleRepository.deleteById(articleId);
+		return "redirect:bulletin-board";
+	}
+
+}
